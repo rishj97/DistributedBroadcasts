b77220357a039b0e540724ae3c1512aa42b58889
Rishabh Jain <wakeuprj@gmail.com>
2018-01-28 20:17:12 +0000

    eh

--- System1/lib/peer.ex
+++ System1/lib/peer.ex
@@ -10,6 +10,7 @@ def start(num) do
 end
 
 defp wait_for_broadcast(num, peers) do
+    # List with data about number of broadcasts sent/received per peer
     peer_data = for peer <- peers, do: {peer, {0,0}}
   receive do
       { :broadcast, max_messages, timeout } ->
@@ -17,32 +18,35 @@ defp wait_for_broadcast(num, peers) do
   end
 end
 
-defp broadcast(peer_data, num_left, end_time, my_num) do
+defp broadcast(peer_data, broadcasts_left, end_time, my_num) do
     cond do
         cur_time() >= end_time ->
-            print_result(my_num, peer_data)
-        num_left > 0 ->
+            stop_broadcasting(my_num, peer_data)
+        broadcasts_left > 0 ->
             receive do
                 {:broadcast_msg, peer_from} ->
                     peer_data = receive_broadcast(peer_from, peer_data)
-                    broadcast(peer_data, num_left, end_time, my_num)
+                    broadcast(peer_data, broadcasts_left, end_time, my_num)
             after
+                # Incase nothing to be received
                 0 ->
                     peer_data = send_broadcast(peer_data)
-                    broadcast(peer_data, num_left - 1, end_time, my_num)
+                    broadcast(peer_data, broadcasts_left - 1, end_time, my_num)
             end
         true ->
             receive do
                 {:broadcast_msg, peer_from} ->
                     peer_data = receive_broadcast(peer_from, peer_data)
-                    broadcast(peer_data, num_left, end_time, my_num)
+                    broadcast(peer_data, broadcasts_left, end_time, my_num)
             after
-                0 -> broadcast(peer_data, num_left, end_time, my_num)
+                # Recursive call to recheck timeout
+                0 -> broadcast(peer_data, broadcasts_left, end_time, my_num)
             end
     end
 end
 
-defp print_result(num, peer_data) do
+defp stop_broadcasting(num, peer_data) do
+    # Display results
     str = for {_, stats} <- peer_data do
         " #{inspect(stats)}"
     end
@@ -51,9 +55,9 @@ defp print_result(num, peer_data) do
 end
 
 defp send_broadcast(peer_data) do
-    for {peer, {sent, received}} <- peer_data do
+    for {peer, {num_sent, num_received}} <- peer_data do
         send peer, {:broadcast_msg, self()}
-        {peer, {sent + 1, received}}
+        {peer, {num_sent + 1, num_received}}
     end
 end
 
@@ -63,6 +67,7 @@ defp receive_broadcast(peer_from, peer_data) do
         peer_data, peer_from, 0, {peer_from, {num_sent, num_received + 1}})
 end
 
+# Returns current time in milliseconds
 defp cur_time() do
     :os.system_time(:millisecond)
 end
